6426925de7e552d89fee68efd0b412fd1cb57342
firmanh3200 <olah3273@gmail.com>
2025-08-27 09:38:22 +0700

    menambah konten monitoring

--- script.js
+++ script.js
@@ -63,6 +63,36 @@ const tabData = {
             'Accordion Item #4 for Tab Six',
             'Accordion Item #5 for Tab Six'
         ]
+    },
+    tab7: {
+        title: 'Tab Six',
+        accordions: [
+            'Accordion Item #1 for Tab Six',
+            'Accordion Item #2 for Tab Six',
+            'Accordion Item #3 for Tab Six',
+            'Accordion Item #4 for Tab Six',
+            'Accordion Item #5 for Tab Six'
+        ]
+    },
+    tab8: {
+        title: 'Tab Six',
+        accordions: [
+            'Accordion Item #1 for Tab Six',
+            'Accordion Item #2 for Tab Six',
+            'Accordion Item #3 for Tab Six',
+            'Accordion Item #4 for Tab Six',
+            'Accordion Item #5 for Tab Six'
+        ]
+    },
+    tab9: {
+        title: 'Tab Six',
+        accordions: [
+            'Accordion Item #1 for Tab Six',
+            'Accordion Item #2 for Tab Six',
+            'Accordion Item #3 for Tab Six',
+            'Accordion Item #4 for Tab Six',
+            'Accordion Item #5 for Tab Six'
+        ]
     }
 };
 
